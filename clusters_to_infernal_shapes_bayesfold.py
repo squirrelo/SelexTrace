88e37d1a6faa042b6cfe410d6c8193c7be8c2c7b
Joshua Shorenstein <joshua.shorenstein@colorado.edu>
2013-05-16 22:13:33 -0600

    Runtime arguments file output added

--- clusters_to_infernal_shapes_bayesfold.py
+++ clusters_to_infernal_shapes_bayesfold.py
@@ -278,6 +278,20 @@ if __name__ == "__main__":
         otufolder += "/"
     if not exists(otufolder):
         mkdir(otufolder)
+
+    #print out run info to a file
+    infofile = open(otufolder + "runparams.txt", 'w')
+    infofile.write( ''.join(["FASTA file:\t\t\t\t", args.i, "\n",
+                    "Base folder:\t\t\t", args.f, "\n",
+                    "Output folder:\t\t\t", args.o, "\n"
+                    "Selection round:\t\t", str(args.r), "\n",
+                    "Uclust simmilarity:\t\t", str(args.sim), "\n",
+                    "Min seqs for group:\t\t", str(args.minseqs), "\n",
+                    "Iterations for grouping:", str(args.iter), "\n",
+                    "RNAForester min score:\t", str(args.rsc), "\n",
+                    "Infernal min score:\t\t", str(args.isc), "\n",
+                    "CPUs:\t\t\t\t\t", str(args.c), "\n"]))
+    infofile.close()
     print "==Clustering sequences by primary sequence=="
     clusters = {}
     secs = time()
